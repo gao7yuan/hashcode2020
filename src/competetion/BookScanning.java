dfa5cb14e4601ec9bf526635fa4ede2957fa6ed0
gao7yuan <gao7yuan@gmail.com>
2020-02-20 10:43:23 -0800

    signature

--- src/competetion/BookScanning.java
+++ src/competetion/BookScanning.java
@@ -1,5 +1,15 @@
 package competetion;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class BookScanning {
+
+  Set<Integer> scanned = new HashSet<>();
+
+  int scan(int[] scores, Library[] libraries, int days) {
+    return 0;
+  }
 
 }
